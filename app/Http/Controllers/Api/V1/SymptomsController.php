50aef4c82f4cac35a7e599f8d97661f949467ffd
matheusmurray <matheus.murray@wolk.com.br>
2023-07-05 01:24:38 -0300

    feat: edit informed symptom

--- app/Http/Controllers/Api/V1/SymptomsController.php
+++ app/Http/Controllers/Api/V1/SymptomsController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\Api\V1\SuccessResource;
 use App\Models\PatientSymptoms;
 use App\Models\Symptoms;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SymptomsController extends Controller
 {
@@ -23,6 +24,25 @@ class SymptomsController extends Controller
         }
     }
 
+    public function informedSymptomEdit(Request $request)
+    {
+        try {
+
+            $return = PatientSymptoms::whereId($request['informed_symptom_id'])->update(
+                [
+                    "symptom_scale" => $request['symptom_scale'],
+                    "diagnoses_id" => $request['diagnoses_id'],
+                    "symptom_id" => $request['symptom_id']
+                ]
+            );
+
+            return response()->json(new SuccessResource("Sintoma informado editado com sucesso!"), 200);
+
+        } catch (\Throwable $th) {
+            return response()->json(new ErrorResource("Erro ao informar sintoma!"), 422);
+        }
+    }
+
     public function informedSymptoms(Request $request)
     {
         try {
@@ -30,7 +50,7 @@ class SymptomsController extends Controller
             $symptoms = PatientSymptoms::
                 where('created_at', '>', $request['start_time'])->
                 where('created_at', '<', $request['end_time'])->
-                where('patient_id', $request['patient_id'])->
+                where('patient_id', Auth::id())->
                 where('symptom_id', $request['symptom_id'])->
                 where('diagnoses_id', $request['diagnoses_id'])->get()->toArray();
 
