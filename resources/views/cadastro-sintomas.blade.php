862a5f24003a7f1de3dd2fcf2261f13ca580b99e
matheusmurray <matheus.murray@wolk.com.br>
2023-07-07 12:24:04 -0300

    fix: corrija identação

--- resources/views/cadastro-sintomas.blade.php
+++ resources/views/cadastro-sintomas.blade.php
@@ -24,7 +24,7 @@
                 </div>
             </div>
             <div id="tabela-medicamentos" class="h-5/6 my-20 rounded-xl shadow">
-            <table class="table-auto w-full h-full text-xs rounded-xl">
+                <table class="table-auto w-full h-full text-xs rounded-xl">
                     <thead class="h-16">
                         <tr class="bg-gray-800">
                             <th class="rounded-tl-xl px-4">
@@ -45,38 +45,62 @@
                         <tr>
                             <td class="px-4">1</td>
                             <td class="px-4">Febre</td>
-                            <td class="px-4 underline decoration-blue-400 decoration-2"><p class="flex justify-end">Editar</p></td>
-                            <td class="px-4 underline decoration-pink-400 decoration-2 w-1/12"><p class="flex justify-start">Excluir</p></td>
+                            <td class="px-4 underline decoration-blue-400 decoration-2">
+                                <p class="flex justify-end">Editar</p>
+                            </td>
+                            <td class="px-4 underline decoration-pink-400 decoration-2 w-1/12">
+                                <p class="flex justify-start">Excluir</p>
+                            </td>
                         <tr>
                             <td class="px-4">2</td>
                             <td class="px-4">Náusea</td>
-                            <td class="px-4 underline decoration-blue-400 decoration-2"><p class="flex justify-end">Editar</p></td>
-                            <td class="px-4 underline decoration-pink-400 decoration-2 w-1/12"><p class="flex justify-start">Excluir</p></td>
+                            <td class="px-4 underline decoration-blue-400 decoration-2">
+                                <p class="flex justify-end">Editar</p>
+                            </td>
+                            <td class="px-4 underline decoration-pink-400 decoration-2 w-1/12">
+                                <p class="flex justify-start">Excluir</p>
+                            </td>
                         </tr>
                         <tr>
                             <td class="px-4">3</td>
                             <td class="px-4">Refluxo</td>
-                            <td class="px-4 underline decoration-blue-400 decoration-2"><p class="flex justify-end">Editar</p></td>
-                            <td class="px-4 underline decoration-pink-400 decoration-2 w-1/12"><p class="flex justify-start">Excluir</p></td>
+                            <td class="px-4 underline decoration-blue-400 decoration-2">
+                                <p class="flex justify-end">Editar</p>
+                            </td>
+                            <td class="px-4 underline decoration-pink-400 decoration-2 w-1/12">
+                                <p class="flex justify-start">Excluir</p>
+                            </td>
                         </tr>
                         <tr>
                             <td class="px-4">4</td>
                             <td class="px-4">Tosse</td>
-                            <td class="px-4 underline decoration-blue-400 decoration-2"><p class="flex justify-end">Editar</p></td>
-                            <td class="px-4 underline decoration-pink-400 decoration-2 w-1/12"><p class="flex justify-start">Excluir</p></td>
+                            <td class="px-4 underline decoration-blue-400 decoration-2">
+                                <p class="flex justify-end">Editar</p>
+                            </td>
+                            <td class="px-4 underline decoration-pink-400 decoration-2 w-1/12">
+                                <p class="flex justify-start">Excluir</p>
+                            </td>
                         </tr>
                         <tr>
                             <td class="px-4">5</td>
                             <td class="px-4">Secreção Nasal</td>
-                            <td class="px-4 underline decoration-blue-400 decoration-2"><p class="flex justify-end">Editar</p></td>
-                            <td class="px-4 underline decoration-pink-400 decoration-2 w-1/12"><p class="flex justify-start">Excluir</p></td>
+                            <td class="px-4 underline decoration-blue-400 decoration-2">
+                                <p class="flex justify-end">Editar</p>
+                            </td>
+                            <td class="px-4 underline decoration-pink-400 decoration-2 w-1/12">
+                                <p class="flex justify-start">Excluir</p>
+                            </td>
                         </tr>
                         <tr>
                             <td class="px-4">6</td>
                             <td class="px-5">Tremores</td>
-                            <td id="editar" class="px-4 underline decoration-blue-400 decoration-2"><p class="flex justify-end">Editar</p></td>
-                            <td class="px-4 underline decoration-pink-400 decoration-2 w-1/12"><p class="flex justify-start">Excluir</p></td>
-                            </tr>
+                            <td id="editar" class="px-4 underline decoration-blue-400 decoration-2">
+                                <p class="flex justify-end">Editar</p>
+                            </td>
+                            <td class="px-4 underline decoration-pink-400 decoration-2 w-1/12">
+                                <p class="flex justify-start">Excluir</p>
+                            </td>
+                        </tr>
                         </tr>
                         <tr class="bg-gray-800 h-10">
                             <td class="px-4 rounded-bl-xl"></td>
