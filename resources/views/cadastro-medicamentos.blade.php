844bbad6ecac52a90502ff6f21d144a0966ef92f
matheusmurray <matheus.murray@wolk.com.br>
2023-07-24 20:49:08 -0300

    fix: cadastro medicamentos

--- resources/views/cadastro-medicamentos.blade.php
+++ resources/views/cadastro-medicamentos.blade.php
@@ -8,7 +8,7 @@
         <div id="container-content" class="bg-white w-4/5 ml-[20%] p-20 h-screen">
             <div class="header flex justify-between content-center">
                 <div id="titulo-pagina" class="text-gray-900 text-xl font-medium underline">
-                    <a class="flex" href="">
+                    <a class="flex" href="{{ route('login') }}">
                         <div class="w-7 mr-3 flex justify-center">
                             <img src="{{URL::asset('/image/med-icon.svg')}}" alt="profile Pic" height="20" width="20">
                         </div>
@@ -71,7 +71,7 @@
                             <td id="volume_flask-{{ $medicamento->id }}" class="px-4">{{ $medicamento->volume_flask }}</td>
                             <td id="formulation-{{ $medicamento->id }}" class="px-4">{{ $medicamento->formulation }}</td>
                             <td id="lab-{{ $medicamento->id }}" class="px-4">{{ $medicamento->lab }}</td>
-                            <td onClick="alimenteModalMedicamentoEdit({{ $medicamento->id }})" data-modal-target="edit-modal-{{ $medicamento->id }}" data-modal-toggle="edit-modal-{{ $medicamento->id }}" class="px-4 underline decoration-blue-400 decoration-2 hover:text-blue-600 focus:text-red">
+                            <td data-modal-target="edit-modal-{{ $medicamento->id }}" data-modal-toggle="edit-modal-{{ $medicamento->id }}" class="px-4 underline decoration-blue-400 decoration-2 hover:text-blue-600 focus:text-red">
                                 <p class="flex justify-end">Editar</p>
                             </td>
                             <td data-modal-target="delete-modal-{{ $medicamento->id }}" data-modal-toggle="delete-modal-{{ $medicamento->id }}" class="px-4 underline decoration-pink-400 decoration-2 w-1/12 hover:text-red-600 focus:text-red">
@@ -113,39 +113,36 @@
                         </div>
                         <!-- Modal body -->
                         <div class="p-6 space-y-6">
-                            <p class="text-base leading-relaxed text-gray-500">
                             <div>
-                                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900">Nome Medicamento</label>
-                                <input type="text" name="brand" id="input-add-name" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
+                                <label for="input-add-name" class="block mb-2 text-sm font-medium text-gray-900">Nome Medicamento</label>
+                                <input type="text" id="input-add-name" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
                             </div>
                             <div>
-                            <div>
-                        <label for="category" class="block mb-2 text-sm font-medium text-gray-900">Apresentação</label>
-                        <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
-                            <option selected="">ml</option>
-                            <option value="TV">gotas</option>
-                            <option value="PC">comprimidos</option>
-                            <option value="GA">mg</option>
-                            <option value="PH">g</option>
-                        </select>
-                            <div>
-                                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900">Concentração</label>
-                                <input type="text" name="brand" id="input-add-concentration" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
+                                <label for="input-add-presentation" class="block mb-2 text-sm font-medium text-gray-900">Apresentação</label>
+                                <select id="input-add-presentation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
+                                    <option selected="ml">ml</option>
+                                    <option value="gotas">gotas</option>
+                                    <option value="comprimidos">comprimidos</option>
+                                    <option value="mg">mg</option>
+                                    <option value="g">g</option>
+                                </select>
                             </div>
                             <div>
-                                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900">Volume Frasco</label>
-                                <input type="text" name="brand" id="input-add-volume_flask" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
+                                <label for="input-add-concentration" class="block mb-2 text-sm font-medium text-gray-900">Concentração</label>
+                                <input type="text" id="input-add-concentration" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
                             </div>
                             <div>
-                                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900">Formulação</label>
-                                <input type="text" name="brand" id="input-add-formulation" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
+                                <label for="input-add-volume_flask" class="block mb-2 text-sm font-medium text-gray-900">Volume Frasco</label>
+                                <input type="text" id="input-add-volume_flask" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
                             </div>
                             <div>
-                                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900">Laboratório</label>
-                                <input type="text" name="brand" id="input-add-lab" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
+                                <label for="input-add-formulation" class="block mb-2 text-sm font-medium text-gray-900">Formulação</label>
+                                <input type="text" id="input-add-formulation" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
                             </div>
-
-                            </p>
+                            <div>
+                                <label for="input-add-lab" class="block mb-2 text-sm font-medium text-gray-900">Laboratório</label>
+                                <input type="text" id="input-add-lab" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
+                            </div>
                         </div>
                         <!-- Modal footer -->
                         <div class="flex justify-end items-center p-6 space-x-2 border-t border-gray-200 rounded-b">
@@ -171,35 +168,32 @@
                             </button>
                         </div>
                         <!-- Modal body -->
-
+    
                         <div class="p-6 space-y-6">
-                            <p class="text-base leading-relaxed text-gray-500">
                             <div>
-                                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900">Nome Medicamento</label>
-                                <input type="text" name="brand" id="input-name-{{ $medicamento->id }}" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Exemplo: Febre">
+                                <label for="input-name-{{ $medicamento->id }}" class="block mb-2 text-sm font-medium text-gray-900">Nome Medicamento</label>
+                                <input type="text" id="input-name-{{ $medicamento->id }}" value="{{ $medicamento->name }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
                             </div>
                             <div>
-                                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900">Apresentação</label>
-                                <input type="text" name="brand" id="input-presentation-{{ $medicamento->id }}" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Exemplo: Febre">
+                                <label for="input-presentation-{{ $medicamento->id }}" class="block mb-2 text-sm font-medium text-gray-900">Apresentação</label>
+                                <input type="text" id="input-presentation-{{ $medicamento->id }}" value="{{ $medicamento->presentation }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
                             </div>
                             <div>
-                                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900">Concentração</label>
-                                <input type="text" name="brand" id="input-concentration-{{ $medicamento->id }}" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Exemplo: Febre">
+                                <label for="input-concentration-{{ $medicamento->id }}" class="block mb-2 text-sm font-medium text-gray-900">Concentração</label>
+                                <input type="text" id="input-concentration-{{ $medicamento->id }}" value="{{ $medicamento->concentration }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
                             </div>
                             <div>
-                                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900">Volume Frasco</label>
-                                <input type="text" name="brand" id="input-volume_flask-{{ $medicamento->id }}" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Exemplo: Febre">
+                                <label for="input-volume_flask-{{ $medicamento->id }}" class="block mb-2 text-sm font-medium text-gray-900">Volume Frasco</label>
+                                <input type="text" id="input-volume_flask-{{ $medicamento->id }}" value="{{ $medicamento->volume_flask }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
                             </div>
                             <div>
-                                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900">Formulação</label>
-                                <input type="text" name="brand" id="input-formulation-{{ $medicamento->id }}" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Exemplo: Febre">
+                                <label for="input-formulation-{{ $medicamento->id }}" class="block mb-2 text-sm font-medium text-gray-900">Formulação</label>
+                                <input type="text" id="input-formulation-{{ $medicamento->id }}" value="{{ $medicamento->formulation }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
                             </div>
                             <div>
-                                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900">Laboratório</label>
-                                <input type="text" name="brand" id="input-lab-{{ $medicamento->id }}" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Exemplo: Febre">
+                                <label for="input-lab-{{ $medicamento->id }}" class="block mb-2 text-sm font-medium text-gray-900">Laboratório</label>
+                                <input type="text" id="input-lab-{{ $medicamento->id }}" value="{{ $medicamento->lab }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="">
                             </div>
-
-                            </p>
                         </div>
                         <!-- Modal footer -->
                         <div class="flex justify-end items-center p-6 space-x-2 border-t border-gray-200 rounded-b">
