5c93d93efb741184060c374336d78ee6d729f252
cainanaraujo <cainandropa1301@outlook.com>
2024-01-25 11:54:09 -0300

    fix/command-in-console

--- app/Console/Commands/SendMedicineReminders.php
+++ app/Console/Commands/SendMedicineReminders.php
@@ -3,26 +3,28 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Models\MedicineReminder;
 
 class SendMedicineReminders extends Command
 {
-    protected $signature = 'send:medicine-reminders';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-medicine-reminders';
 
-    protected $description = 'Send medicine reminders';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
 
+    /**
+     * Execute the console command.
+     */
     public function handle()
     {
-        $currentTime = now()->format('H:i');
-
-        $medicineReminders = MedicineReminder::whereRaw("TIME_FORMAT(reminder_time, '%H:%i') = ?", [$currentTime])->get();
-
-        foreach ($medicineReminders as $medicineReminder) {
-//ok
-
-            $this->info("Sending reminder for MedicineReminder ID: $medicineReminder->id");
-        }
-
-        $this->info('Medicine reminders sent successfully.');
+        //
     }
 }
